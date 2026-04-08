bedb707543bf23e6a0cd46a4d1c54f45581d75f6
kim-kiwi <s26018@gsm.hs.kr>
2026-04-08 17:38:38 +0900

    last

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,11 +1,11 @@
 import { Backend } from "../../route";
 
-const Method: Backend['method'] = 'all';
-const Handler: Backend['handler'] = async (req, res) => {
-    return res.json({ success: true, message: 'blog.lnst.kr', isLogined: req.session.isLogined });
+const Method: Backend["method"] = "all";
+const Handler: Backend["handler"] = async (req, res) => {
+  return res.json({ success: true, message: "blog.lnst.kr" });
 };
 
 export default {
-    method: Method,
-    handler: Handler
+  method: Method,
+  handler: Handler,
 } as Backend;
